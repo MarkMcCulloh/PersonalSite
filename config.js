a5d4f93926b35c11e0b578770c27fbb833a5b8c6
Mark McCulloh <Mark.McCulloh@gmail.com>
2019-11-18 18:57:23 -0500

    Move to a blog style. First PoC

---
+++ config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  url: 'https://markmcculloh.com',
+  pathPrefix: '/',
+  title: 'Mark McCulloh',
+  subtitle: 'Mark McCulloh\'s Personal Site.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Blog',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Mark',
+    photo: '/photo.jpg',
+    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    contacts: {
+      email: 'Mark.McCulloh@gmail.com',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: ''
+    }
+  }
+};
