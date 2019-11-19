6b114c31b63a2fbf0deedba2209991335ae7f960
Mark McCulloh <Mark.McCulloh@gmail.com>
2019-11-18 19:51:01 -0500

    Fix config again

--- config.js
+++ config.js
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'Mark',
+    name: 'Mark McCulloh',
     photo: '/media/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Platform Engineer and Automation Fanatic',
     contacts: {
       email: 'Mark.McCulloh@gmail.com',
       github: 'https://github.com/MarkMcCulloh',
@@ -42,6 +42,44 @@ module.exports = {
       gitlab: '',
       weibo: '',
       codepen: ''
+    }
+  }
+};
+
+'use strict';
+
+module.exports = {
+  url: 'https://markmcculloh.com',
+  pathPrefix: '/',
+  title: 'Mark McCulloh',
+  subtitle: 'Mark McCulloh\'s Personal Site.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Blog',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Mark McCulloh',
+    photo: '/photo.jpg',
+    bio: 'Platform Engineer and Automation Fanatic',
+    contacts: {
+      email: 'Mark.McCulloh@gmail.com',
+      github: 'https://github.com/MarkMcCulloh',
+      linkedin: 'https://www.linkedin.com/in/mark-mcculloh/'
     }
   }
 };
